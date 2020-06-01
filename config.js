a166c33dcfa8c9d48ef0073d62fe015fc028b5a7
vlinr <282204626@qq.com>
2020-06-01 16:40:17 +0800

    修复错误内容

--- config.js
+++ config.js
@@ -1,6 +1,6 @@
 const Config = {
     maxComNum:500, //个人账号的最大压缩数量
-    dir:'CompressImage'  //本地图片来源，默认为当前目录下的CompressImage
+    dir:'resource'  //本地图片来源，默认为当前目录下的CompressImage
 }
 module.exports = {
     Config
--- index.js
+++ index.js
@@ -1,10 +1,11 @@
 /******
  * 
- * 系统版本：1.0.1
+ * 系统版本：1.0.2
  * 系统作者：keen
- * 系统说明：本系统接入tp压缩，key需要自己去官方申请，进入根目录直接node index，然后输入key值即可自动压缩，config为一些配置，
- * 默认输出目录会在CompressImage，安装放入的原样输出，讲需要压缩的图片放到CompressImage目录下，压缩完成后，会自动放回对应目录。
+ * 系统说明：本系统基于tinypng压缩，key需要自己去官方申请，进入根目录直接node index，然后输入key值即可自动压缩，config为一些配置，
+ * 默认输出目录会在resource，按照放入的原样输出，将需要压缩的图片放到resource目录下，压缩完成后，会自动放回对应目录。
  * 完成时间：2020-05-19
+ * 更新时间：2020-06-01
  * E-mail:zlife@vip.qq.com
  * *****/
 const tinify = require("tinify"),
@@ -13,7 +14,7 @@ const tinify = require("tinify"),
 Config = require("./config").Config;
 class Compress {
     constructor(key, path) {
-        console.log('压缩服务已启动~');
+        console.log('zlife@vip.qq.com：服务已启动...');
         this.source = [];
         this.allnum = 0;
         this.init(key, path);
@@ -28,7 +29,7 @@ class Compress {
     }
     //设置秘钥
     init(key, path) {
-        console.log('key初始化成功，等待签名认证...');
+        console.log('初始化成功，等待校验...');
         tinify.key = key;
         this.validateKey(path);
     }
@@ -51,6 +52,7 @@ class Compress {
             //计算完毕
             this.compress(checkNumber);
         });
+
     }
     //计算文件总数
     computedFiles(path) {
@@ -81,19 +83,19 @@ class Compress {
     compress(surplus) {
         let fileNum = this.getFileNum();
         console.log('文件总数为：' + fileNum);
-        if(fileNum > surplus){
+        if (fileNum > surplus) {
             console.log('压缩次数剩余不足，建议更换key或减少图数量,文件数：' + fileNum + '-----剩余压缩次数：' + surplus);
             process.exit(0);
         }
         console.log('开始压缩文件，请等待...');
         this.source.map(async item => {
             if (!this.checkFileType(item.name, ['png', 'jpg', 'jpeg'])) {
-                console.log('压缩失败,文件类型错误,名称为：' + item.name + '-----剩余：'+this.allnum);
+                console.log('压缩失败,文件类型错误,名称为：' + item.name + '-----剩余：' + this.allnum);
                 this.allnum--;
             } else {
                 const source = await tinify.fromFile(item.path);  //来自于哪里
                 await source.toFile(item.path);  //输出到哪里
-                console.log('压缩成功,名称为：' + item.name + '-----剩余：'+this.allnum);
+                console.log('压缩成功,名称为：' + item.name + '-----剩余：' + this.allnum);
                 this.allnum--;
             }
             if (this.allnum == 0) {
@@ -116,6 +118,7 @@ const readLine = readline.createInterface({
     input: process.stdin,
     output: process.stdout
 });
+//获得输入
 readLine.on('line', function (line) {
     new Compress(`${line.replace(/\s+/g, '')}`, Config.dir);
 });
